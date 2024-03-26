454eb014e790e6b3111db2238494b8057b969833
avantikakende <140887896+avantikakende@users.noreply.github.com>
2024-03-26 08:31:55 +0530

    Add files via upload

---
+++ First.js
@@ -0,0 +1,76 @@
+// 1. Take two numbers a,b and print the sum, difference, product, quotient and modulus of a & b.
+// var a = 10;
+// var b = 3;
+// console.log("Sum:", a + b);
+// console.log("Difference:", a - b);
+// console.log("Product:", a * b);
+// console.log("Quotient:", a / b);
+// // console.log("Modulus:", a % b);
+
+ 
+// 2. Take two numbers and only print the integer part when a is divided by b.
+// const a = 10;
+// const b = 3;
+// console.log(a%b);
+
+
+// 3. Take two numbers a,b and print the exponential power of the first number raised by the second.
+// const a = 2;
+// const b = 3;
+// const result = a ** b;
+// console.log(result);
+
+
+// 4. Take two numbers a,b and only print the decimal part of the result obtained when a is divided by b.
+// let a=35;
+// let b=6;
+// console.log(a/b);
+
+
+// 5. Take marks in 5 subjects A,B,C,D,E and print out the average of marks from five subject marks.
+// let a=57;
+// let b=19;
+// let c=20;
+// let d=55;
+// let e=90;
+// console.log(a+b+c+d+e/5);
+
+
+// 6. Take the cost price a and selling price b of a product, and print the profit obtained on the product.
+// let cp=44;
+// let sp=50;
+// console.log(cp-sp);
+
+
+// 7. Take a number and print out the last digit of the number.
+// var a=77;
+// console.log(a%10);
+
+
+// 8. Take a 2 digit number and return the first and last digits.
+// WRONG QUESTION CORRECT IT .
+// let num1=34;
+// let num2=65;
+// let num3=9;
+// let lnum=lnum(num1,num2,num3);
+// console.log("lnum:",lnum);
+
+
+// 10. Write a JavaScript program to check whether a number is even or odd.
+// var n=12;
+// if(n%2==0){
+//     console.log("even");
+// }
+// else{
+//     console.log("odd");
+// }
+
+
+// 11. Write a JavaScript program to check whether a given number is prime or not.
+// let n=21;
+// if(n>1){
+//     console.log("prime");
+// }
+// else{
+//     console.log("not prime");
+// }
